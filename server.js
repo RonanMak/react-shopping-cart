1532ad76547c6c2b49634eadbc81962d41b24214
Ronan Mak <ronanmak@macbook-pro-2.mynet>
2021-03-05 11:32:34 +0800

    adding

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-// first open the DB by using
-// mongod --dbpath ./mongo-data/
-// endpoint: http://localhost:5000/api/products
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -10,14 +6,11 @@ const shortid = require("shortid");
 const app = express();
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost/Eshop", {
+mongoose.connect("mongodb://localhost/react-shopping-cart-db", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
-
-//define porduct model. Mongoose.model() is responsible for creating a model and it accepts two parameters
-// 1 param : the name of this collection inside the DB. 2: the list of fields of this model into the DB
 
 const Product = mongoose.model(
   "products",
@@ -31,28 +24,72 @@ const Product = mongoose.model(
   })
 );
 
-//the endpoint is req res and the body of function
 app.get("/api/products", async (req, res) => {
-  //find takes no param, it means there is no condition and return all products
-  //the .find() is the promise. To get the data we need to use the new syntax of async await function. make the function async
   const products = await Product.find({});
   res.send(products);
 });
 
-//create products
 app.post("/api/products", async (req, res) => {
-  //sending a req fron frontend to this endpoint we need to fill req.body with that data of the new product
   const newProduct = new Product(req.body);
   const savedProduct = await newProduct.save();
   res.send(savedProduct);
 });
 
 app.delete("/api/products/:id", async (req, res) => {
-  //the function comes from the product model.  req.params.id = :id
   const deletedProduct = await Product.findByIdAndDelete(req.params.id);
   res.send(deletedProduct);
 });
 
-// 5000 is offically variable default 5000
+const Order = mongoose.model(
+  "order",
+  new mongoose.Schema(
+    {
+      _id: {
+        type: String,
+        default: shortid.generate,
+      },
+      email: String,
+      name: String,
+      address: String,
+      total: Number,
+      cartItems: [
+        {
+          _id: String,
+          title: String,
+          price: Number,
+          count: Number,
+        },
+      ],
+    },
+    {
+      timestamps: true,
+    }
+  )
+);
+
+app.post("/api/orders", async (req, res) => {
+  if (
+    !req.body.name ||
+    !req.body.email ||
+    !req.body.address ||
+    !req.body.total ||
+    !req.body.cartItems
+  ) {
+    return res.send({ message: "Data is required." });
+  }
+  const order = await Order(req.body).save();
+  res.send(order);
+});
+
+app.get("/api/orders", async (req, res) => {
+  const orders = await Order.find({});
+  res.send(orders);
+});
+
+app.delete("/api/orders/:id", async (req, res) => {
+  const order = await Order.findByIdAndDelete(req.params.id);
+  res.send(order);
+});
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log("server at http://localhost:5000"));
+app.listen(port, () => console.log("serve at http://localhost:5000"));
--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,10 +1,7 @@
 import { FETCH_PRODUCTS } from "../types";
 import { FILTER_PRODUCTS_BY_SIZE, ORDER_PRODUCTS_BY_PRICE } from "../types";
-
 export const fetchProducts = () => async (dispatch) => {
   const res = await fetch("/api/products");
-  // the data comes from Fitch method need to be converted to JSON
-  // below ... the JSON file return Promose, make it await
   const data = await res.json();
   console.log(data);
   dispatch({
@@ -13,11 +10,9 @@ export const fetchProducts = () => async (dispatch) => {
   });
 };
 
-//takes two params 1. all products 2.the sizes we need to filter
 export const filterProducts = (products, size) => (dispatch) => {
   dispatch({
     type: FILTER_PRODUCTS_BY_SIZE,
-    // gonna send an object and it contains two value. 1. the selected size and filtered products
     payload: {
       size: size,
       items:
@@ -27,22 +22,17 @@ export const filterProducts = (products, size) => (dispatch) => {
     },
   });
 };
-
 export const sortProducts = (filteredProducts, sort) => (dispatch) => {
   const sortedProducts = filteredProducts.slice();
-  // "" means sorted by _id
   if (sort === "latest") {
     sortedProducts.sort((a, b) => (a._id > b._id ? 1 : -1));
-    // sorted by price
   } else {
     sortedProducts.sort((a, b) =>
       sort === "lowest"
-        ? //lowest to highest
-          a.price > b.price
+        ? a.price > b.price
           ? 1
           : -1
-        : //highest to lowest
-        a.price > b.price
+        : a.price > b.price
         ? -1
         : 1
     );
